d971dd6b6f0e49a08a97600a8289aa4e7b9a2b30
Elena Rodriguez <helenarodcal@gmail.com>
2021-10-26 18:56:40 +0200

    ExceptionHandlingExercise

--- src/main/java/com/serenitydojo/exceptions/MissingWelcomeFileException.java
+++ src/main/java/com/serenitydojo/exceptions/MissingWelcomeFileException.java
@@ -1,7 +1,8 @@
 package com.serenitydojo.exceptions;
 
 public class MissingWelcomeFileException extends RuntimeException {
-    public MissingWelcomeFileException(String message, Throwable e) {
-        super(message, e);
+
+    public MissingWelcomeFileException(String message) {
+        super(message);
     }
 }
